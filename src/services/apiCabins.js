80691b54db5aa8ed116bb914e7d7859f7df18d11
SabinaCristea <sabinacristea997@gmail.com>
2024-01-25 15:14:31 +0200

    setup of react query and react hook form to create and delete cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,3 +10,30 @@ export async function getCabins() {
 
   return data;
 }
+
+// From API docs in Supabase
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could non be created");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could non be deleted");
+  }
+
+  return data;
+}
