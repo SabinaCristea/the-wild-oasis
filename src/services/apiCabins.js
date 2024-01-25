5e1bee6b3c93fb50b2741a1ca9af5010a1e8d8da
SabinaCristea <sabinacristea997@gmail.com>
2024-01-25 20:15:59 +0200

    created a new cabin, handled form errors, uploaded images of cabin to supabase

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,11 +1,11 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   let { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
     console.error(error);
-    throw new Error("Cabins could non be loaded");
+    throw new Error("Cabins could not be loaded");
   }
 
   return data;
@@ -14,14 +14,38 @@ export async function getCabins() {
 // From API docs in Supabase
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // https://usioiuiauydnbpfwfawx.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
+
+  // 1. Create cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
     console.error(error);
-    throw new Error("Cabins could non be created");
+    throw new Error("Cabins could not be created");
+  }
+
+  // 2. Upload image --- from supabase
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin ifthere was an error uploading the image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created"
+    );
   }
 
   return data;
@@ -32,7 +56,7 @@ export async function deleteCabin(id) {
 
   if (error) {
     console.error(error);
-    throw new Error("Cabins could non be deleted");
+    throw new Error("Cabins could not be deleted");
   }
 
   return data;
